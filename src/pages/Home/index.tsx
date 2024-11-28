dcdc43fe75b05f71d0ccd3c544c20e5a4447995c
hindami <hindami050294@gmail.com>
2024-11-28 13:28:41 +0700

    feat: Home component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,79 @@
+import { Address } from '@ton/core';
+import { useTonConnectUI } from '@tonconnect/ui-react';
+import { useCallback, useEffect, useState } from 'react';
+
+export default function Home() {
+  const [tonConnectUI] = useTonConnectUI();
+  const [tonWalletAddress, setTonWalletAddress] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleWalletConnection = useCallback((address: string) => {
+    setTonWalletAddress(address);
+    console.log('Wallet connected successfully!');
+    setIsLoading(false);
+  }, []);
+
+  const handleWalletDisconnection = useCallback(() => {
+    setTonWalletAddress(null);
+    console.log('Wallet disconnected successfully!');
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const checkWalletConnection = async () => {
+      if (tonConnectUI.account?.address) {
+        handleWalletConnection(tonConnectUI.account?.address);
+      } else {
+        handleWalletDisconnection();
+      }
+    };
+
+    checkWalletConnection();
+
+    const unsubscribe = tonConnectUI.onStatusChange((wallet) => {
+      if (wallet) {
+        handleWalletConnection(wallet.account.address);
+      } else {
+        handleWalletDisconnection();
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [tonConnectUI, handleWalletConnection, handleWalletDisconnection]);
+
+  const handleWalletAction = async () => {
+    if (tonConnectUI.connected) {
+      setIsLoading(true);
+      await tonConnectUI.disconnect();
+    } else {
+      await tonConnectUI.openModal();
+    }
+  };
+
+  const formatAddress = (address: string) => {
+    const tempAddress = Address.parse(address).toString();
+    return `${tempAddress.slice(0, 4)}...${tempAddress.slice(-4)}`;
+  };
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <>
+      <h1>Ton connect demo</h1>
+      {tonWalletAddress ? (
+        <>
+          <p>Connected: {formatAddress(tonWalletAddress)}</p>
+          <button onClick={handleWalletAction}>Disconnect Wallet</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleWalletAction}>Connect TON Wallet</button>
+        </>
+      )}
+    </>
+  );
+}
